27cd00cf3f642b1cd9c2bcc3508b52900553bd27
Scott Bender <scott@scottbender.net>
2026-03-27 16:22:09 -0400

    feature: add ability to respond with error from put handlers

---
+++ signalk-put-success.js
@@ -0,0 +1,20 @@
+
+module.exports = function(RED) {
+  function SignalK(config) {
+    RED.nodes.createNode(this,config);
+    var node = this;
+
+    var app = node.context().global.get('app')
+
+    node.on('input', msg => {
+      let cb = msg.putCallBack
+      if ( cb ) {
+        cb({ state: 'COMPLETED', statusCode: msg.payload?.statusCode || 200, message: msg.payload?.message || 'OK'})
+      } else {
+        node.error('No callback provided for put response')
+      }
+    })
+  }
+  RED.nodes.registerType("signalk-put-success", SignalK);
+}
+
